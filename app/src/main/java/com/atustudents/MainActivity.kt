bf751fc4e4cb9e20c77e301bd9b3f669531aead0
Jimale <jimale@tiriig.com>
2024-05-04 09:51:09 +0300

    Added ImageView

--- app/src/main/java/com/atustudents/MainActivity.kt
+++ app/src/main/java/com/atustudents/MainActivity.kt
@@ -2,27 +2,48 @@ package com.atustudents
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.transition.Visibility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        displayStudentName()
+        imageViewSample()
     }
 
-    private fun displayStudentName(){
+    private fun imageViewSample() {
+        val imageView: ImageView = findViewById(R.id.student_pic)
+        val editText: EditText = findViewById(R.id.student_name_et)
+        val button: Button = findViewById(R.id.save_btn)
+
+        imageView.setOnClickListener {
+            editText.visibility = View.GONE
+            button.visibility = View.GONE
+        }
+
+        button.setOnClickListener {
+            imageView.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.image_two_24)
+            )
+        }
+    }
+
+    private fun displayStudentName() {
 
 //        val textView : TextView = findViewById(R.id.student_name_tv)
-        val editText : EditText = findViewById(R.id.student_name_et)
-        val button : Button = findViewById(R.id.save_btn)
+        val editText: EditText = findViewById(R.id.student_name_et)
+        val button: Button = findViewById(R.id.save_btn)
 
 
         button.setOnClickListener {
@@ -31,8 +52,8 @@ class MainActivity : AppCompatActivity() {
             //textView.text = textFromEditText
 
             //Starting new activity
-            val intent = Intent(this,DisplayActivity::class.java)
-            intent.putExtra("student_name",textFromEditText)
+            val intent = Intent(this, DisplayActivity::class.java)
+            intent.putExtra("student_name", textFromEditText)
             startActivity(intent)
 
 
